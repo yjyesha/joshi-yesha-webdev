a263a2baf99023d9b730e61e0a94e985c4f8ff4e
yjyesha <yj.yesha@gmail.com>
2017-07-05 23:29:10 -0400

    First Commit

---
+++ server.js
@@ -0,0 +1,11 @@
+var app = require('./express');
+var express = app.express;
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(__dirname + '/public'));
+
+require("./test/app");
+
+app.listen(3000);
