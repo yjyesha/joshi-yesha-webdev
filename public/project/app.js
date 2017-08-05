629b6806ff37a2e0b860fcb7c9bfdfd49f2d3e84
yjyesha <yj.yesha@gmail.com>
2017-08-04 20:33:54 -0400

    POC addition of geolocation

--- public/project/app.js
+++ public/project/app.js
@@ -44,7 +44,8 @@
         var model = this;
 
         model.searchFoodSpotByCity = searchFoodSpotByCity;
-
+        model.searchFoodSpotByLocation = searchFoodSpotByLocation;
+        model.geoFindMe = geoFindMe;
         function init() {
             console.log("ko");
         }
@@ -56,13 +57,50 @@
                 .then(renderFoodSpots);
         }
 
+        function searchFoodSpotByLocation() {
+            foodService
+                .searchFoodSpotByLocation()
+                .then(renderFoodSpots);
+        }
+
         function renderFoodSpots(response) {
             model.movies = response.data;
+        }
+
+        function geoFindMe() {
+            var output = document.getElementById("out");
+            console.log("lkuh");
+            if (!navigator.geolocation){
+                console.log("ink")
+                output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+                return;
+            }
+
+            function success(position) {
+                var latitude  = position.coords.latitude;
+                var longitude = position.coords.longitude;
+
+                output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
+
+                var textnode = document.createTextNode("We found you..");
+
+                output.appendChild(textnode);
+                searchFoodSpotByLocation(latitude,longitude);
+            }
+
+            function error() {
+                output.innerHTML = "Unable to retrieve your location";
+            }
+
+            output.innerHTML = "<p>Locating…</p>";
+
+            navigator.geolocation.getCurrentPosition(success, error);
         }
     }
 
     function foodService($http) {
         this.searchFoodSpotByCity = searchFoodSpotByCity;
+        this.searchFoodSpotByLocation = searchFoodSpotByLocation;
         this.searchMovieByImdbId = searchMovieByImdbId;
 
         function searchMovieByImdbId(imdbID) {
@@ -71,6 +109,14 @@
                 .then(function (response) {
                     return response.data;
                 });
+        }
+
+
+        function searchFoodSpotByLocation(lat,lng) {
+            console.log("ehhh");
+            return $http.get('https://api.yelp.com/v3/businesses/search?term=food&latitude='+lat+'&longitude='+lng, {
+                headers: {'Content-Type':'application/x-www-form-urlencoded','Authorization': 'Bearer ZTE-0HHqsbQLo0zbqEAOvDuFKplK6l1N1zTw7T3_w-Hzja7x1VdenPu1-lA1P82VDjogZeywIoLN_WdOxFxJdt3Isl14v1Re73YOWiNOrB2H_4b5Ozg-LBwi3--DWXYx'}
+            });
         }
 
         function searchFoodSpotByCity(city) {
