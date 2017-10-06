26112e54dac3bf968417a9570cfcd8361ba8c7bb
yjyesha <yj.yesha@gmail.com>
2017-10-06 14:01:23 -0400

    Removed Project, Refactored Homepage

--- public/project/Foodie/clientSide/app.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by yeshajoshi on 7/17/2017.
- */
-(function () {
-    //iife
-    angular.module("WebAppMaker", ["ngRoute"])
-})();
--- public/project/Foodie/serverSide/app.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by yeshajoshi on 7/17/2017.
- */
-require("../serverSide/services/user.service.server");
-require("../serverSide/services/eatSpot.service.server");
-require("../serverSide/services/service.server");
